eb5b782e4d16b000f323854ff7d3ec86cf75ad95
Yonhoo <cookies_yon@163.com>
2022-06-26 14:21:41 +0800

    [feat] : add main description

--- src/main/java/com/example/jooq/JooqApplication.java
+++ src/main/java/com/example/jooq/JooqApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JooqApplication {
 
+	// main method
 	public static void main(String[] args) {
 		SpringApplication.run(JooqApplication.class, args);
 	}
